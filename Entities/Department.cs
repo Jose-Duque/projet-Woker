a1f0eb17e41c544c77bffceda14ecea4307767f6
Jose Carlos Duque <cj_duque@hotmail.com>
2020-04-07 18:48:48 -0300

    Projeto Workers

---
+++ Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proj_Worker.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+        public Department (string name)
+        {
+            Name = name;
+        }
+
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proj_Worker.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        senior = 2,
+    };
+}
